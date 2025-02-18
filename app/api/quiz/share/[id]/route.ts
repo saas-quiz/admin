01b2807c97d6ea573bffebc8ae4f1c0a01a6d853
sandeep kumar <sandeepypb@gmail.com>
2025-02-18 19:07:50 +0530

    ✨ Feat: shuffle questions

--- app/api/quiz/share/[id]/route.ts
+++ app/api/quiz/share/[id]/route.ts
@@ -2,6 +2,14 @@ import { auth } from "@/auth";
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+const shuffleArray = (array: any[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 export async function GET(req: NextRequest) {
   try {
     const id = req.url.split("/").pop();
@@ -18,7 +26,10 @@ export async function GET(req: NextRequest) {
       return NextResponse.json({ ok: true, error: "This link is not valid!", quiz }, { status: 400 });
     }
 
-    return NextResponse.json({ ok: true, message: "Data fetched successfully", quiz }, { status: 200 });
+    return NextResponse.json(
+      { ok: true, message: "Data fetched successfully", quiz: { ...quiz, questions: shuffleArray(quiz.questions) } },
+      { status: 200 }
+    );
   } catch (error: any) {
     console.error(error?.message);
     return NextResponse.json({ ok: false, error: "Something went wrong" }, { status: 500 });
